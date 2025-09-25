ae5748f49b11303e99a1b2973f0e977d4d5bff05
sweet-smart <alewidavid178@gmail.com>
2025-09-25 15:12:03 +0100

    second commit

--- routes/event.js
+++ routes/event.js
@@ -5,6 +5,7 @@ import {
   updateEvent,
   deleteEvent,
   rsvpEvent,
+  getEventById
 } from "../controller/event.js";
 import { verifyUser } from "../middleware/auth.js";
 import { isOrganizerOrAdmin } from "../middleware/admin.js";
@@ -14,8 +15,9 @@ const router = express.Router();
 // Event routes
 router.post("/", verifyUser, isOrganizerOrAdmin, createEvent);
 router.get("/", getEvents);
-router.put("/:id", verifyUser, isOrganizerOrAdmin, updateEvent);
+router.put("/:id", verifyUser, isOrganizerOrAdmin, updateEvent);    // 👈 Added missing comma
 router.delete("/:id", verifyUser, isOrganizerOrAdmin, deleteEvent);
 router.post("/:id/rsvp", verifyUser, rsvpEvent);
+router.get("/:id", getEventById);  // 👈 Added missing import and route
 
 export default router; // 👈 This fixes your error
